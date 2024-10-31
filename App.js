14cf31cadffa7c5a2543316e48d644c1e2e238f3
PhamVanThanh <phamtrung21112003@gmail.com>
2024-10-31 11:39:29 +0700

    upload ResetEmail, SignUp, LoginByPhoneNumber

--- App.js
+++ App.js
@@ -7,19 +7,25 @@ import WalkThrough02 from './javascript/ScreenWalkThrough02'
 import WalkThrough03 from './javascript/ScreenWalkThrough03'
 import SignIn from './javascript/ScreenSignIn'
 import ForgotPassword from './javascript/ForgotPassword'
+import ResetEmail from './javascript/ResetEmail'
+import SignUp from './javascript/SignUp'
+import LoginByPhoneNumber from './javascript/LoginByPhoneNumber'
 
 const Stack = createStackNavigator();
 
 const YourApp = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Navigator initialRouteName="LoginByPhoneNumber">
         <Stack.Screen name="ScreenWelcome" component={ScreenWelcome} />
         <Stack.Screen name="WalkThrough01" component={WalkThrough01} />
         <Stack.Screen name="WalkThrough02" component={WalkThrough02} />
         <Stack.Screen name="WalkThrough03" component={WalkThrough03} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="ResetEmail" component={ResetEmail} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="LoginByPhoneNumber" component={LoginByPhoneNumber} />
       </Stack.Navigator>
     </NavigationContainer>
   );

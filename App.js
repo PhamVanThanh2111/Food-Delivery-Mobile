eafe29b2dfa6f8b90bbac42fed2e1ccf5b5e60ef
PhamVanThanh <phamtrung21112003@gmail.com>
2024-10-31 09:45:52 +0700

    upload Forgot password

--- App.js
+++ App.js
@@ -6,18 +6,20 @@ import WalkThrough01 from './javascript/ScreenWalkThrough01'
 import WalkThrough02 from './javascript/ScreenWalkThrough02'
 import WalkThrough03 from './javascript/ScreenWalkThrough03'
 import SignIn from './javascript/ScreenSignIn'
+import ForgotPassword from './javascript/ForgotPassword'
 
 const Stack = createStackNavigator();
 
 const YourApp = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Navigator initialRouteName="ForgotPassword">
         <Stack.Screen name="ScreenWelcome" component={ScreenWelcome} />
         <Stack.Screen name="WalkThrough01" component={WalkThrough01} />
         <Stack.Screen name="WalkThrough02" component={WalkThrough02} />
         <Stack.Screen name="WalkThrough03" component={WalkThrough03} />
         <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       </Stack.Navigator>
     </NavigationContainer>
   );
